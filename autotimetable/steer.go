4f243a00d6da25a51b6915971701a6464448bca6
gradgrind <mt.bothfeld@gmail.com>
2025-08-26 09:03:24 +0200

    Working on constraint disabling

--- autotimetable/steer.go
+++ autotimetable/steer.go
@@ -11,78 +11,10 @@ import (
 
 // TODO: At present this only supports a FET back-end. Perhaps a choice should
 // be possible ...
-func SteerGeneration(tt_data *timetable.TtData, stempath string) {
-	// Create a fresh working directory
-	workingdir := stempath + "_fet"
-	os.RemoveAll(workingdir)
-	err := os.Mkdir(workingdir, 0755)
-	if err != nil && !os.IsExist(err) {
-		panic(err)
-	}
-
-	run_number := 0
-	// Each run of FET is in its own subdirectory
-
-	run_number++
-	dir_n := filepath.Join(workingdir, "run_"+strconv.Itoa(run_number))
-	err = os.Mkdir(dir_n, 0755)
-	if err != nil && !os.IsExist(err) {
-		panic(err)
-	}
-	stemfile := filepath.Join(dir_n, filepath.Base(stempath))
-	fetfile := stemfile + ".fet"
-	mapfile := stemfile + ".map"
-	xmlitem, lessonIdMap := fet.MakeFetFile(tt_data)
-
-	// Write FET file
-	f, err := os.Create(fetfile)
-	if err != nil {
-		base.Bug.Fatalf("Couldn't open output file: %s\n", fetfile)
-	}
-	defer f.Close()
-	_, err = f.WriteString(xmlitem)
-	if err != nil {
-		base.Bug.Fatalf("Couldn't write fet output to: %s\n", fetfile)
-	}
-	base.Message.Printf("FET file written to: %s\n", fetfile)
-
-	// Write Id-map file.
-	fm, err := os.Create(mapfile)
-	if err != nil {
-		base.Bug.Fatalf("Couldn't open output file: %s\n", mapfile)
-	}
-	defer fm.Close()
-	_, err = fm.WriteString(lessonIdMap)
-	if err != nil {
-		base.Bug.Fatalf("Couldn't write fet output to: %s\n", mapfile)
-	}
-	base.Message.Printf("Id-map written to: %s\n", mapfile)
-
-	base.Message.Println("OK")
-
-	// First run with no constraints except the hard-blocked time slots and
-	// the fixed activities.
-
-	/* ???
-
-	// Add the pseudo activities due to the NotAvailable lists of classes,
-	// teachers and rooms.
-	tt_data.BlockResources()
-
-	TODO?
-	// Get preliminary constraint info – needed for the call to addActivity
-	//ttinfo.processConstraints()
-
-	TODO?
-	// Add the remaining Activity information
-	//ttinfo.addActivityInfo(t2tt, r2tt, g2ags)
-
-	*/
-}
-
+//
 /*
-// TODO: This was the original code to build the .fet and .map files.
-// Is the bit fetching file names from db.ModuleData still needed somehow?
+// TODO:  Is this old bit fetching file names from db.ModuleData still
+// needed somehow?
 
 	fetfile := stempath
 	mapfile := stempath
@@ -102,33 +34,133 @@ func SteerGeneration(tt_data *timetable.TtData, stempath string) {
 	}
 	fetfile += ".fet"
 	mapfile += ".map"
+*/
 
+var run_number int
+
+func SteerGeneration(tt_data *timetable.TtData, stempath string) {
+	// `stempath` provides the path to the source file, including the stem
+	// (without file-type extension) of the file name. A new working directory
+	// will be created in the same directory.
+	workingdir := stempath + "_fet"
+	os.RemoveAll(workingdir)
+	err := os.Mkdir(workingdir, 0755)
+	if err != nil && !os.IsExist(err) {
+		panic(err)
+	}
+
+	run_number = 0 // each run of FET is in its own subdirectory
+	db := tt_data.Db
+	db_0 := *db // save original data (shallow copy only!)
+	//TODO: From tt_data I probably just need to save the following
+	tt_data_constraints := tt_data.Constraints
+	tt_data_mindaysbetweenlessons := tt_data.MinDaysBetweenLessons
+	tt_data_parallellessons := tt_data.ParallelLessons
+	tt_data_withoutroomplacements := tt_data.WITHOUT_ROOM_PLACEMENTS
+	tt_data.Constraints = map[string][]any{}
+	tt_data.MinDaysBetweenLessons = nil
+	tt_data.ParallelLessons = nil
+	tt_data.WITHOUT_ROOM_PLACEMENTS = true // TODO: not working?
+
+	// First run with no constraints except the hard-blocked time slots and
+	// the fixed activities.
+	// Clear teachers' constraints
+	new_teachers := make([]*base.Teacher, len(db.Teachers))
+	for i, t0p := range db.Teachers {
+		t := *t0p
+		t.MinLessonsPerDay = -1 // unconstrained
+		t.MaxLessonsPerDay = -1 // unconstrained
+		t.MaxDays = -1          // unconstrained
+		t.MaxGapsPerDay = -1    // unconstrained
+		t.MaxGapsPerWeek = -1   // unconstrained
+		t.MaxAfternoons = -1    // unconstrained
+		t.LunchBreak = false
+		new_teachers[i] = &t
+	}
+	db.Teachers = new_teachers
+	// Clear class constraints
+	new_classes := make([]*base.Class, len(db.Classes))
+	for i, c0p := range db.Classes {
+		c := *c0p
+		c.MinLessonsPerDay = -1 // unconstrained
+		c.MaxLessonsPerDay = -1 // unconstrained
+		c.MaxGapsPerDay = -1    // unconstrained
+		c.MaxGapsPerWeek = -1   // unconstrained
+		c.MaxAfternoons = -1    // unconstrained
+		c.LunchBreak = false
+		c.ForceFirstHour = false
+		new_classes[i] = &c
+	}
+	db.Classes = new_classes
+
+	//TODO: Bug? Still getting students max gaps per week 100% 0 days!
+
+	//TODO... ???
+
+	runFET(tt_data, workingdir)
+
+	//TODO: Restore original data
+	tt_data.Constraints = tt_data_constraints
+	tt_data.MinDaysBetweenLessons = tt_data_mindaysbetweenlessons
+	tt_data.ParallelLessons = tt_data_parallellessons
+	tt_data.WITHOUT_ROOM_PLACEMENTS = tt_data_withoutroomplacements
+	tt_data.Db = &db_0
+
+	/* ???
+
+	// Add the pseudo activities due to the NotAvailable lists of classes,
+	// teachers and rooms.
+	tt_data.BlockResources()
+
+	TODO?
+	// Get preliminary constraint info – needed for the call to addActivity
+	//ttinfo.processConstraints()
+
+	TODO?
+	// Add the remaining Activity information
+	//ttinfo.addActivityInfo(t2tt, r2tt, g2ags)
+
+	*/
+}
+
+func runFET(tt_data *timetable.TtData, workingdir string) {
+	run_number++
+	fname := "run_" + strconv.Itoa(run_number)
+	dir_n := filepath.Join(workingdir, fname)
+	err := os.Mkdir(dir_n, 0755)
+	if err != nil && !os.IsExist(err) {
+		panic(err)
+	}
+	stemfile := filepath.Join(dir_n, fname)
+	fetfile := stemfile + ".fet"
+	mapfile := stemfile + ".map"
+
+	// Construct the FET-file
 	xmlitem, lessonIdMap := fet.MakeFetFile(tt_data)
 
 	// Write FET file
 	f, err := os.Create(fetfile)
 	if err != nil {
-		base.Bug.Fatalf("Couldn't open output file: %s\n", fetfile)
+		panic("Couldn't open output file: " + fetfile)
 	}
 	defer f.Close()
 	_, err = f.WriteString(xmlitem)
 	if err != nil {
-		base.Bug.Fatalf("Couldn't write fet output to: %s\n", fetfile)
+		panic("Couldn't write fet output to: " + fetfile)
 	}
 	base.Message.Printf("FET file written to: %s\n", fetfile)
 
 	// Write Id-map file.
 	fm, err := os.Create(mapfile)
 	if err != nil {
-		base.Bug.Fatalf("Couldn't open output file: %s\n", mapfile)
+		panic("Couldn't open output file: " + mapfile)
 	}
 	defer fm.Close()
 	_, err = fm.WriteString(lessonIdMap)
 	if err != nil {
-		base.Bug.Fatalf("Couldn't write fet output to: %s\n", mapfile)
+		panic("Couldn't write fet output to: " + mapfile)
 	}
 	base.Message.Printf("Id-map written to: %s\n", mapfile)
 
 	base.Message.Println("OK")
-
-*/
+}
